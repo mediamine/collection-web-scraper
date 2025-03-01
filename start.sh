752ba4847171307c5aa3e0485533860a64f00bfa
MohsinKhan-MediaMine <mohsin@mediamine.co.nz>
2025-03-02 07:15:10 +1300

    feat: script to execute the scraping flow in loop

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Need to provide execute rights to this script before executing it locally
+# $1 = no. of iterations
+INPUT_NO_OF_ITERATIONS=$1
+NO_OF_ITERATIONS=${INPUT_NO_OF_ITERATIONS:-10}
+
+for i in {1..10}
+    do
+        echo "start iteration: $i";
+        WORKFLOW=WORKFLOW_COMPLETE_SCAN yarn start;
+        sleep 3600;
+        WORKFLOW=WORKFLOW_PAGE_TEXT_SCAN yarn start;
+        sleep 3600;
+        WORKFLOW=WORKFLOW_RSS_SCAN yarn start;
+        sleep 3600;
+        echo "end iteration: $i";
+    done
